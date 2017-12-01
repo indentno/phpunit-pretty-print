06f5347449d6053e5031a18b3d5e866cdd658baa
ecrmnn <danieleckermann@gmail.com>
2017-12-01 12:03:53 +0100

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# phpunit-pretty-print
+> Prettify PHPUnit output
+
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+[![eslint](https://img.shields.io/badge/code_style-PSR_2-blue.svg?style=flat-square)](http://www.php-fig.org/psr/psr-2/)
+
+### Installation
+```bash
+composer require sempro/phpunit-pretty-print --dev
+```
+
+### Usage
+You can specify the printer to use on the phpunit command line:
+
+```bash
+php vendor/bin/phpunit --printer 'Sempro\PHPUnitPrettyPrinter\PrettyPrinter' tests/
+```
+
+Optionally, you can add it to your project's `phpunit.xml` file instead:
+
+```xml
+<phpunit
+    bootstrap="bootstrap.php"
+    colors="true"
+    printerClass="Sempro\PHPUnitPrettyPrinter\PrettyPrinter">
+```
+
+### License
+MIT © [Sempro AS](http://www.sempro.no)
---
+++ src/PrettyPrinter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Sempro\PHPUnitPrettyPrinter;
+
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestFailure;
+use PHPUnit\Framework\TestListener;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\TextUI\ResultPrinter;
+use PHPUnit\Util\Filter;
+
+class PrettyPrinter extends ResultPrinter implements TestListener
+{
+    protected $className;
+    protected $previousClassName;
+
+    public function startTestSuite(TestSuite $suite)
+    {
+        parent::startTestSuite($suite);
+    }
+
+    public function startTest(Test $test)
+    {
+        $this->className = get_class($test);
+    }
+
+    public function endTest(Test $test, $time)
+    {
+        parent::endTest($test, $time);
+
+        $testMethodName = explode('::', \PHPUnit\Util\Test::describe($test));
+        preg_match_all('/((?:^|[A-Z])[a-z]+)/', $testMethodName[1], $matches);
+
+        $testNameArray = array_map('strtolower', $matches[0]);
+        array_shift($testNameArray);
+        $name = implode(' ', $testNameArray);
+
+        $color = 'fg-green';
+        if ($test->getStatus() !== 0) {
+            $color = 'fg-red';
+        }
+
+        $this->write(' ');
+        $this->writeWithColor($color, $name, false);
+        $this->write(' ');
+        $this->writeWithColor('fg-white', '[' . number_format($time, 3) . 's]', true);
+    }
+
+    protected function writeProgress($progress)
+    {
+        if ($this->previousClassName !== $this->className) {
+            $this->write("\n");
+            $this->writeWithColor('bold', $this->className, false);
+            $this->writeNewLine();
+        }
+
+        $this->previousClassName = $this->className;
+
+        if ($progress == '.') {
+            $this->writeWithColor('fg-green', '  ✓', false);
+        } else {
+            $this->writeWithColor('fg-red', '  x', false);
+        }
+    }
+
+    protected function printDefectTrace(TestFailure $defect)
+    {
+        $this->write($this->formatExceptionMsg($defect->getExceptionAsString()));
+        $trace = Filter::getFilteredStacktrace(
+            $defect->thrownException()
+        );
+        if (!empty($trace)) {
+            $this->write("\n" . $trace);
+        }
+        $exception = $defect->thrownException()->getPrevious();
+        while ($exception) {
+            $this->write(
+                "\nCaused by\n" .
+                TestFailure::exceptionToString($e) . "\n" .
+                Filter::getFilteredStacktrace($e)
+            );
+            $exception = $exception->getPrevious();
+        }
+    }
+
+    protected function formatExceptionMsg($exceptionMessage)
+    {
+        $exceptionMessage = str_replace("+++ Actual\n", '', $exceptionMessage);
+        $exceptionMessage = str_replace("--- Expected\n", '', $exceptionMessage);
+        $exceptionMessage = str_replace('@@ @@', '', $exceptionMessage);
+
+        if ($this->colors) {
+            $exceptionMessage = preg_replace('/^(Exception.*)$/m', "\033[01;31m$1\033[0m", $exceptionMessage);
+            $exceptionMessage = preg_replace('/(Failed.*)$/m', "\033[01;31m$1\033[0m", $exceptionMessage);
+            $exceptionMessage = preg_replace("/(\-+.*)$/m", "\033[01;32m$1\033[0m", $exceptionMessage);
+            $exceptionMessage = preg_replace("/(\++.*)$/m", "\033[01;31m$1\033[0m", $exceptionMessage);
+        }
+
+        return $exceptionMessage;
+    }
+}
