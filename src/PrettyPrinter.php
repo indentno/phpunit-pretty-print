9e6006823a014464bcb3773923667387df1640dd
Pierre Goiffon <pierre.goiffon@combodo.com>
2023-01-06 14:05:03 +0100

    :bulb: Add phpdoc to PrettyPrinter class
    See #32

--- src/PrettyPrinter.php
+++ src/PrettyPrinter.php
@@ -5,6 +5,13 @@ namespace Sempro\PHPUnitPrettyPrinter;
 use PHPUnit\Framework\TestListener;
 use PHPUnit\TextUI\ResultPrinter;
 
+/**
+ * For PHPUnit9 you must use instead {@see PrettyPrinterForPhpUnit9}
+ *
+ * @deprecated 1.4.0
+ *
+ * @link https://github.com/indentno/phpunit-pretty-print/issues/32 Fix for PHPUnit9 compatibility
+ */
 class PrettyPrinter extends ResultPrinter implements TestListener
 {
     use PrettyPrinterTrait;
